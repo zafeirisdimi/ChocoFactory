ed93fae282fce3f26c983fb80327103e874bff13
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-05-12 15:49:29 +0300

    Add company policy property and RevenueGoalAchieved method.

--- ChocoFactory/Domain/Company.cs
+++ ChocoFactory/Domain/Company.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChocoFactory.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,17 @@ namespace ChocoFactory.Domain
     internal class Company
     {
         public decimal Capital { get; set; } = 1000000;
+        public decimal Revenue { get; set; }
         public List<Factory> Factories { get; set; } = new List<Factory>();
         public List<Shop> Shops { get; set; } = new List<Shop>();
         public List<Employee> Employees { get; set; } = new List<Employee>();
+        public CompanyPolicy CompanyPolicy { get; set; }
+
+
+        public bool RevenueGoalAchieved()
+        {
+            return Revenue > (decimal)CompanyPolicy.RevenueYearlyGoal * Revenue;
+        }
 
     }
 }
