6e3301dd75f7ea9fbb03fd684fb6487d11891fcb
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-05-13 02:30:02 +0300

    Changed Order implementation.

--- ChocoFactory/Domain/Order.cs
+++ ChocoFactory/Domain/Order.cs
@@ -6,49 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChocoFactory.Domain
 {
-    public abstract class Order
+    class Order : Offer
     {
-        //properties of object Order
-        public int OrderID { get; set; }
-        public string ShortDescription { get; set; }
-
-        public List<Product> Products = new List<Product>();
-        public double FinalCost { get; set; }
-        public string PayMethod { get; set; }
-
-
-
-       
-        //custom constructor
-        protected Order(int orderID, string shortDescription, List<Product> products, double finalCost, string payMethod)
-        {
-            OrderID = orderID;
-            ShortDescription = shortDescription;
-            Products = products;
-            FinalCost = finalCost;
-            PayMethod = payMethod;
-        }
-
-        
-
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        //methods
-
-
     }
 }
