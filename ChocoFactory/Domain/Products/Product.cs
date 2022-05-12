b46233f49eb8020c6fc29416d16a862c1da5bb01
StaurosGouleas <steve_goul@hotmail.com>
2022-05-13 00:54:37 +0300

    Update Product.cs

--- ChocoFactory/Domain/Products/Product.cs
+++ ChocoFactory/Domain/Products/Product.cs
@@ -32,25 +32,3 @@ namespace ChocoFactory.Domain
 
 
     }
-}
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChocoFactory.Domain
-{
-    public abstract class Product
-    {
-        //properties
-        public int ProductID { get; set; }
-        public string Description { get; set; }
-        public double Price { get; set; }
-        public DateTime ProductionDate { get; set; }
-        public DateTime ExpirationDate { get; set; }
-
-
-    }
-}
-master
