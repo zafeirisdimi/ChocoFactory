af65c93d2ef2f773abc1361989d9b7d4f13a53b3
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-05-13 01:39:22 +0300

    Added properties to classes.

--- ChocoFactory/Domain/Company.cs
+++ ChocoFactory/Domain/Company.cs
@@ -14,7 +14,7 @@ namespace ChocoFactory.Domain
         public List<Factory> Factories { get; set; } = new List<Factory>();
         public List<Shop> Shops { get; set; } = new List<Shop>();
         public List<Employee> Employees { get; set; } = new List<Employee>();
-        public CompanyPolicy CompanyPolicy { get; set; }
+        public CompanyPolicy CompanyPolicy { get; set; } = new CompanyPolicy();
         public bool RevenueGoalAchieved
         {
             get { return Revenue > (decimal)CompanyPolicy.RevenueYearlyGoal * Revenue; ; }
