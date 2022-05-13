56ce13d86bc6eb2ba75c0775853a52bd81eb21db
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-05-13 15:59:41 +0300

    Refactored CreateProduct class.

--- ChocoFactory/Domain/Company/Production.cs
+++ ChocoFactory/Domain/Company/Production.cs
@@ -23,43 +23,53 @@ namespace ChocoFactory.Domain
             {
                 case "BlackChocolate":
                     Factory.Warehouse.SendSupplies(ProductionPolicy.BlackChocolateSupplies);
-                    createdProduct = new BlackChocolate();
-                    createdProduct.Description = "BlackChocolate";
-                    createdProduct.ProductionDate = DateTime.Now;
-                    createdProduct.ExpirationDate = DateTime.Now.AddDays(90);
-                    createdProduct.Price = Factory.Company.CompanyPolicy.BlackChocolatePrice;
+                    createdProduct = new BlackChocolate()
+                    {
+                        Description = "BlackChocolate",
+                        ProductionDate = DateTime.Now,
+                        ExpirationDate = DateTime.Now.AddDays(90),
+                        Price = Factory.Company.CompanyPolicy.BlackChocolatePrice
+                    };
                     break;
                 case "WhiteChocolate":
                     Factory.Warehouse.SendSupplies(ProductionPolicy.WhiteChocolateSupplies);
-                    createdProduct = new WhiteChocolate();
-                    createdProduct.Description = "WhiteChocolate";
-                    createdProduct.ProductionDate = DateTime.Now;
-                    createdProduct.ExpirationDate = DateTime.Now.AddDays(60);
-                    createdProduct.Price = Factory.Company.CompanyPolicy.WhiteChocolatePrice;
+                    createdProduct = new WhiteChocolate()
+                    {
+                        Description = "WhiteChocolate",
+                        ProductionDate = DateTime.Now,
+                        ExpirationDate = DateTime.Now.AddDays(60),
+                        Price = Factory.Company.CompanyPolicy.WhiteChocolatePrice
+                    };
                     break;
                 case "PlainMilkChocolate":
                     Factory.Warehouse.SendSupplies(ProductionPolicy.MilkChocolateSupplies);
-                    createdProduct = new PlainMilkChocolate();
-                    createdProduct.Description = "PlainMilkChocolate";
-                    createdProduct.ProductionDate = DateTime.Now;
-                    createdProduct.ExpirationDate = DateTime.Now.AddDays(75);
-                    createdProduct.Price = Factory.Company.CompanyPolicy.MilkChocolatePrice;
+                    createdProduct = new PlainMilkChocolate()
+                    {
+                        Description = "PlainMilkChocolate",
+                        ProductionDate = DateTime.Now,
+                        ExpirationDate = DateTime.Now.AddDays(75),
+                        Price = Factory.Company.CompanyPolicy.MilkChocolatePrice
+                    };
                     break;
                 case "AlmondMilkChocolate":
                     Factory.Warehouse.SendSupplies(ProductionPolicy.AlmondMilkChocolateSupplies);
-                    createdProduct = new AlmondMilkChocolate();
-                    createdProduct.Description = "AlmondMilkChocolate";
-                    createdProduct.ProductionDate = DateTime.Now;
-                    createdProduct.ExpirationDate = DateTime.Now.AddDays(75);
-                    createdProduct.Price = Factory.Company.CompanyPolicy.AlmondMilkChocolatePrice;
+                    createdProduct = new AlmondMilkChocolate()
+                    {
+                        Description = "AlmondMilkChocolate",
+                        ProductionDate = DateTime.Now,
+                        ExpirationDate = DateTime.Now.AddDays(75),
+                        Price = Factory.Company.CompanyPolicy.AlmondMilkChocolatePrice
+                    };
                     break;
                 case "HazelnutMilkChocolate":
                     Factory.Warehouse.SendSupplies(ProductionPolicy.HazelnutMilkChocolateSupplies);
-                    createdProduct = new HazelnutMilkChocolate();
-                    createdProduct.Description = "HazelnutMilkChocolate";
-                    createdProduct.ProductionDate = DateTime.Now;
-                    createdProduct.ExpirationDate = DateTime.Now.AddDays(75);
-                    createdProduct.Price = Factory.Company.CompanyPolicy.HazelnutMilkChocolatePrice;
+                    createdProduct = new HazelnutMilkChocolate()
+                    {
+                        Description = "HazelnutMilkChocolate",
+                        ProductionDate = DateTime.Now,
+                        ExpirationDate = DateTime.Now.AddDays(75),
+                        Price = Factory.Company.CompanyPolicy.HazelnutMilkChocolatePrice
+                    };
                     break;
                 default:
                     Console.WriteLine("Error creating product");
