23ae136f74eb5873a465d0d3020378d225d9b314
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-05-19 22:50:44 +0300

    Changed the namespace to match the other chocolates.

--- ChocoFactory/Domain/Products/WhiteChocolate.cs
+++ ChocoFactory/Domain/Products/WhiteChocolate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChocoFactory.Domain.Products
+namespace ChocoFactory.Domain
 {
     public class WhiteChocolate : IChocolate
     {
