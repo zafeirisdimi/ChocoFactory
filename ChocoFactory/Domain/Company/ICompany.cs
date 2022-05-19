407117f8ec0f97e79520ca43265197b566af4cf4
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-05-19 21:32:28 +0300

    Deleted properties from ICompany.

--- ChocoFactory/Domain/Company/ICompany.cs
+++ ChocoFactory/Domain/Company/ICompany.cs
@@ -8,11 +8,6 @@ namespace ChocoFactory.Domain
     {
         decimal Capital { get; }
         decimal Revenue { get; set; }
-        List<Factory> Factories { get; set; }
-        List<Shop> Shops { get; set; }
-        List<IEmployeeModel> Employees { get; set; }
-        CompanyPolicy CompanyPolicy { get; set; }
-        bool RevenueGoalAchieved { get; set; }
         void DailyActions(DateTime currentDate);
         void YearlyActions();
 
